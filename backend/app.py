f6b7691ba55e42fb92a9520ee176d2bd81289a4c
samriddhipyakurel666 <samriddhipyakurel666@gmail.com>
2026-04-12 07:15:58 +0545

    model.py connected

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from models import db  
 
 app = Flask(__name__, template_folder="../frontend")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///menu.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route("/")
 def home():
