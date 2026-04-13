6c64baedeff85afe1ee0eba1203d164096c9f821
samriddhipyakurel666 <samriddhipyakurel666@gmail.com>
2026-04-13 14:24:57 +0545

    Add backend app.py updated

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
-from models import db  
+from database import init_db, db
+from models import MenuItem, User, Recommendation
 
 app = Flask(__name__, template_folder="../frontend")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///menu.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+
+# Database configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///menu.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialize database
+init_db(app)
 
 @app.route("/")
 def home():
